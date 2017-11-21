3b5545034197e38ce766197668298650ae7ba18c
Inari <inari_whitebear@fastmail.jp>
2017-11-21 18:09:13 +0100

    Better syncing and make channels interact with buckets/fluid container when rightclicked
    Also actually save the data so fluids don't vanish!

--- src/base/java/com/technicalitiesmc/base/block/BlockChannel.java
+++ src/base/java/com/technicalitiesmc/base/block/BlockChannel.java
@@ -6,7 +6,9 @@ import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
@@ -14,8 +16,10 @@ import net.minecraft.world.World;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTank;
+import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 
 import javax.annotation.Nullable;
 
@@ -26,12 +30,15 @@ public class BlockChannel extends BlockBase implements ITileEntityProvider {
 
     @Override
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, RayTraceResult hit) {
+        //if (world.isRemote) return true;
         if (player.isSneaking()) {
+            if (world.isRemote) return true;
             TileEntity te = world.getTileEntity(pos);
             if (te != null) {
                 TileChannel tc = (TileChannel)te;
                 IFluidHandler tank = tc.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, hit.sideHit);
                 tank.fill(new FluidStack(FluidRegistry.WATER, 500), true);
+                te.markDirty();
             }
         } else {
             TileEntity te = world.getTileEntity(pos);
@@ -39,6 +46,10 @@ public class BlockChannel extends BlockBase implements ITileEntityProvider {
                 TileChannel tc = (TileChannel) te;
                 FluidTank tank = (FluidTank)tc.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, hit.sideHit);
                 System.out.println("Tank: " + tank.getFluidAmount());
+            }
+
+            if (!world.isRemote && FluidUtil.interactWithFluidHandler(player, hand, world, pos, hit.sideHit)) {
+                te.markDirty();
             }
         }
 
--- src/base/java/com/technicalitiesmc/base/tile/TileChannel.java
+++ src/base/java/com/technicalitiesmc/base/tile/TileChannel.java
@@ -1,6 +1,9 @@
 package com.technicalitiesmc.base.tile;
 
 import com.technicalitiesmc.util.block.TileBase;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.PacketBuffer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
@@ -9,12 +12,35 @@ import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTank;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TileChannel extends TileBase implements ITickable {
 
     private final FluidTank tank;
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound) {
+        super.readFromNBT(compound);
+
+        NBTBase nbtTag = compound.getTag("tank");
+        if (compound.hasKey("tank")) {
+            NBTTagCompound nbtTank = (NBTTagCompound) compound.getTag("tank");
+            tank.readFromNBT(nbtTank);
+        }
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+        compound = super.writeToNBT(compound);
+
+        NBTTagCompound nbtTank = new NBTTagCompound();
+        tank.writeToNBT(nbtTank);
+        compound.setTag("tank", nbtTank);
+
+        return compound;
+    }
 
     public TileChannel() {
         tank = new FluidTank(1000);
@@ -60,16 +86,37 @@ public class TileChannel extends TileBase implements ITickable {
 
             float diff = Math.abs(tank.getFluidAmount() - otherTank.getFluidAmount());
 
-            float flow = diff * 0.1f;
-            int finalFlow = (int)Math.ceil(Math.max(flow, 5));
+            float flow = diff * 0.01f;
+            int finalFlow = (int)Math.ceil(Math.min(flow, 5));
 
             if (tank.getFluidAmount() > otherTank.getFluidAmount()) {
                 FluidStack stack = tank.drain(finalFlow, true);
                 otherTank.fill(stack, true);
+                channel.markDirty();
+                markDirty();
             } else {
                 FluidStack stack = otherTank.drain(finalFlow, true);
                 tank.fill(stack, true);
+                channel.markDirty();
+                markDirty();
             }
         }
+    }
+
+    @Override
+    public void writeDescription(PacketBuffer buf) throws IOException {
+        super.writeDescription(buf);
+
+        NBTTagCompound nbt = new NBTTagCompound();
+        tank.writeToNBT(nbt);
+        buf.writeCompoundTag(nbt);
+    }
+
+    @Override
+    public void readDescription(PacketBuffer buf) throws IOException {
+        super.readDescription(buf);
+
+        NBTTagCompound nbt = buf.readCompoundTag();
+        tank.readFromNBT(nbt);
     }
 }
